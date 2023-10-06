0b61d3e7a4be3e1706e0ab10e8995f4744cfcd8e
Tl <linhuu8@gmail.com>
2023-10-06 11:58:59 +0700

    add

---
+++ src/ra/bai4/Test.java
@@ -0,0 +1,9 @@
+package ra.bai4;
+
+public class Test {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Alice");
+        person.setClasses("C03");
+    }
+}
